2b7eeb6f52851fe26b8617a428b8ec340161ed00
Izenikez <62757324+izenik-ez@users.noreply.github.com>
2022-12-05 20:48:47 +0100

    End Game logic

--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -1,33 +1,61 @@
+class Player
+  attr_reader :name, :mark
+  @@num = 0
+  def initialize
+    raise "No more than two players" if @@num > 2
+    @@num+=1
+    @name = "Player #{@@num}"
+    if @@num == 1
+      @mark = "X"
+    else
+      @mark = "O"
+    end
+  end
+
+end
+
 class Board
   
   def initialize
-    @board = Array.new(3){Array.new(3, nil)}
-    @player1 = "X"
-    @player2 = "O"
+    @board = Array.new(3){Array.new(3, nil)}    
   end
 
-  def board
-    @board
+  def move_allowed?(row, col)
+    row < @board.length &&
+    col < @board.length &&
+    @board[row][col] == nil
   end
-
+  
   def move(row, col, player)
-    raise IndexError if row >= @board.length || col >= @board[0].length
-    raise StandardError if @board[row][col] != nil
-    @board[row][col] = player
+    @board[row.to_i][col.to_i] = player
+  end
+
+  def ask_for_move (player)    
+    puts "#{player.name}'s turn!" 
+    move = [0,0]
+    print "ROW: "
+    move[0] = gets.chomp.to_i
+    print "COLUMN: "
+    move[1] = gets.chomp.to_i
+    move
   end
 
   def show_board
     @board.each do |row|
       row.each do |square|
-        print "| #{square==nil ? " " : square} "
+        print "| #{square.class == Player ? square.mark : " "} "
       end
-      print "\n"
+      print "|\n"
     end
   end
 
-  #private  
+  def complete_board?
+    @board.all? do |row|
+      row.all?{|square| square.class == Player}
+    end
+  end
 
-  def any_row? (player)
+  def complete_row? (player)
     @board.each_with_index do |row, index|
       if row.all?{|square| square == player}
         return index
@@ -44,7 +72,7 @@ class Board
     columns
   end
   
-  def any_column? (player)
+  def complete_column? (player)
     for i in 0..@board.length-1
       if get_columns(i).all?{|square| square == player}
         return i
@@ -73,7 +101,7 @@ class Board
     end
   end
 
-  def any_diagonal?(player)
+  def complete_diagonal?(player)
     if diagonal1? player
       1
     elsif diagonal2? player
@@ -84,62 +112,65 @@ class Board
   end
 
   def winner?(player)
-    if any_row?(player) ||
-       any_column?(player) ||
-       any_diagonal?(player)
+    if complete_row?(player) ||
+       complete_column?(player) ||
+       complete_diagonal?(player)
       true
     else
       false
     end
   end
-                
-end
-=begin
-  
 
-  def winner?(player)
-    if any_row_player? player
-       column_player?
+  def winner
+    if winner? @player1
+      player1
+    elsif winner? @player2
+      player2
+    else
+      ""
+    end
   end
-=end
+end
+
+
 
 board = Board.new
+p1 = Player.new
+p2 = Player.new
 
-board.show_board
+winner = false
+row = "" 
+column = ""
 
-board.move(1,1,1)
+until board.complete_board?
 
-board.show_board
+  board.show_board
+    
+  begin
+    movement = board.ask_for_move p1
+  end  until board.move_allowed? movement[0].to_i, movement[1].to_i
+  board.move movement[0], movement[1], p1
+  if board.winner? p1
+    puts "Player 1 WINS!"
+    winner = true
+    break
+  end
 
-board.move(1,0,1)
+  break if board.complete_board?    
+  
+  board.show_board
+  
+  begin
+    movement = board.ask_for_move p2
+  end until board.move_allowed? movement[0].to_i, movement[1].to_i
+  board.move movement[0].to_i, movement[1].to_i, p2  
+  
+  if board.winner? p2
+    puts "Player 2 WINS!"
+    winner = true
+    break
+  end    
+end# until board.complete_board?
 
-board.move(1,2,1)
-puts board.any_row? 1
-board.show_board
-#puts board.row_player? 1,1
+puts "Nobody Wins!" unless winner
 
-board = Board.new
-board.move(0,0,1)
-board.move(1,0,1)
-board.move(2,0,1)
-puts board.any_column? 1
-board.show_board
-
-board = Board.new
-board.move(0,0,1)
-board.move(1,1,1)
-board.move(2,2,1)
-
-puts board.diagonal1? 1
-board.show_board
-
-board = Board.new
-board.move(0,2,1)
-board.move(1,1,1)
-board.move(2,0,1)
-
-puts board.diagonal2? 1
-board.show_board
-
-puts board.winner? 1
-puts board.winner? 2
